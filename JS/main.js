9a67a2f92bd28c712f99a07196d7ce7fa14c735e
Khaled Radwan <khaledradwan96@gmail.com>
2024-08-08 23:02:26 +0300

    edit some style on element

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+const result = document.querySelector('.result span')
+const nums = document.querySelectorAll('.calculator .num')
+const reset = document.getElementById('reset')
+const calc = document.getElementById('calc')
+
+
+console.log(nums)
+
